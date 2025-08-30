b10597ceeaca6f4e49b4f1b89f82e29d71f0ea7b
blondisb <blondisb@uninorte.edu.co>
2025-08-30 17:55:14 -0500

    adding imu testing v3

--- gy521-testing/src/main.cpp
+++ gy521-testing/src/main.cpp
@@ -4,9 +4,9 @@
 
 MPU6050 mpu;
 
-// Pines PWM
 const int motorIzq = 9;
 const int motorDer = 10;
+unsigned long startTime;
 
 // PID
 double setpoint = 0;    // Ángulo objetivo (equilibrio)
@@ -31,6 +31,7 @@ void setup() {
     while (1);
   }
   Serial.println("MPU6050 listo");
+  startTime = millis();
 }
 
 void loop() {
@@ -52,15 +53,20 @@ void loop() {
   int pwmIzq = constrain(150 + output, 0, 255);
   int pwmDer = constrain(150 - output, 0, 255);
 
-  //analogWrite(motorIzq, pwmIzq);
-  //analogWrite(motorDer, pwmDer);
+  if (millis()-startTime < 10000){
+    analogWrite(motorIzq, pwmIzq);
+    analogWrite(motorDer, pwmDer);
+  }else{
+    analogWrite(motorIzq, 0);
+    analogWrite(motorDer, 0);
+  }
 
   // Debug
-  Serial.print("AngX: "); Serial.print(input);
-  Serial.print(" PWM_Izq: "); Serial.print(pwmIzq);
-  Serial.print(" PWM_Der: "); Serial.println(pwmDer);
-  Serial.print("AngY: "); Serial.print(atan2(ax, az) * 180 / PI);
-  Serial.print("AngZ: "); Serial.print(atan2(ay, ax) * 180 / PI);
+  Serial.print("|\tAngX: "); Serial.print(input);
+  Serial.print("|\tPWM_Izq: "); Serial.print(pwmIzq);
+  Serial.print("|\tPWM_Der: "); Serial.println(pwmDer);
+  Serial.print("|\tAngY: "); Serial.print(atan2(ax, az) * 180 / PI);
+  Serial.print("|\tAngZ: "); Serial.print(atan2(ay, ax) * 180 / PI);
 
   delay(10);
 }
