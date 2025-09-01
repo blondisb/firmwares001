ab794cbff2ba255568d005ecce2183b30f477c43
blondisb <blondisb@uninorte.edu.co>
2025-08-31 23:50:32 -0500

    qtpy_gpy521_direct

--- qtpy_testing/src/main.cpp
+++ qtpy_testing/src/main.cpp
@@ -1,21 +1,64 @@
-#include <Adafruit_NeoPixel.h>
+/********************************************************************************************************
+  neopixel_basic:
+  Written by Scott Kildall
+  
+  This will light just a NeoPixel strip of NeoPixels
+  
+---------------------------------------------------------------------------------------------------------
+NeoPixel Information for initializing the strip, below
+  60ma/pixel for current load
+  Parameter 1 = number of pixels in strip
+  Parameter 2 = pin number (most are valid)
+  Parameter 3 = pixel type flags, add together as needed:
+    NEO_KHZ800  800 KHz bitstream (most NeoPixel products w/WS2812 LEDs)
+    NEO_KHZ400  400 KHz (classic 'v1' (not v2) FLORA pixels, WS2811 drivers)
+    NEO_GRB     Pixels are wired for GRB bitstream (most NeoPixel products)
+    NEO_RGB     Pixels are wired for RGB bitstream (v1 FLORA pixels, not v2)
 
-#define PIN_NEOPIXEL 8   // Pin del NeoPixel integrado en QT Py M0
-Adafruit_NeoPixel pixel(1, PIN_NEOPIXEL, NEO_GRB + NEO_KHZ800);
+**********************************************************************************************************/
+#include <Adafruit_NeoPixel.h>
+ 
+// the data pin for the NeoPixels
+int neoPixelPin = 11;
+
+// How many NeoPixels we will be using, charge accordingly
+int numPixels = 60;
+
+// Instatiate the NeoPixel from the ibrary
+Adafruit_NeoPixel strip = Adafruit_NeoPixel(numPixels, neoPixelPin, NEO_GRB + NEO_KHZ800);
+
+// Global RGB values, change to suit your needs
+int r = 0;
+int g = 125;
+int b = 250;
 
 void setup() {
-  Serial.begin(115200);
-  while (!Serial) delay(10);
-  Serial.println("Encendiendo NeoPixel en color fijo...");
-
-  pixel.begin();
-  pixel.setBrightness(150);  // ajusta brillo (0-255)
-
-  // Enciende en rojo
-  pixel.setPixelColor(0, pixel.Color(255, 0, 0));
-  pixel.show();
+  strip.begin();  // initialize the strip
+  strip.show();   // make sure it is visible
+  strip.clear();  // Initialize all pixels to 'off'
 }
 
 void loop() {
-  // Nada que hacer aquí, el LED queda fijo
+   // set the colors for the strip
+   if (b > 250) b=0;
+   else b = b - 30;
+
+   if (r > 250) r=0;
+   else r = r + 30;
+
+   if (g > 250) g=0;
+   else g = g + 30;
+
+   strip.setBrightness(12.5);
+   for( int i = 0; i < numPixels; i++ )
+       strip.setPixelColor(i, r, g, b);
+   
+   // show all pixels  
+   strip.show();
+   
+   // wait for a short amount of time -- sometimes a short delay of 5ms will help
+   // technically we only need to execute this one time, since we aren't changing the colors but we will build on this structure
+   delay(100);
+
+   
 }
